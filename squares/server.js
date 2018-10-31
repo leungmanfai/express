eee3095beeb095ada11471d54f1221f7524d9226
Raymond SO <rso@ouhk.edu.hk>
2018-10-31 17:06:32 +0800

    set HTTP status code

--- squares/server.js
+++ squares/server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/perimeter', function (req, res) {
-  res.send('Answer is '+req.query.length * 4);
+  res.status(200).send('Answer is '+req.query.length * 4);
 });
 
 app.get('/area', function (req, res) {
-  res.send('Answer is '+req.query.length * req.query.length);
+  res.status(200).send('Answer is '+req.query.length * req.query.length);
 });
 
 var server = app.listen(process.env.PORT || 8099, function () {
