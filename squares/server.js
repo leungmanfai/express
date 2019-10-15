40faaca096e8d59b34611f64d17f84c6481adbc6
Raymond So <rso@ouhk.edu.hk>
2019-10-15 11:25:57 +0800

    added query string paramter 'format'

--- squares/server.js
+++ squares/server.js
@@ -2,18 +2,32 @@ let express = require('express');
 let app = express();
 
 app.get('/perimeter', (req,res) => {
-  res.status(200).type('text').send(`Answer is ${req.query.length * 4}`);
+  if (req.query.format === 'json') {
+    let answer = {};
+    answer['length'] = req.query.length;
+    answer['perimeter'] = req.query.length * 4;
+    res.status(200).json(answer);
+  } else {
+    res.status(200).type('text').send(`Answer is ${req.query.length * 4}`);
+  }
 });
 
 app.get('/area', (req,res) => {
-  res.status(200).type('text').send(`Answer is ${req.query.length * req.query.length}`);
+  if (req.query.format === 'json') {
+    let answer = {};
+    answer['length'] = req.query.length;
+    answer['area'] = req.query.length ^ 2;
+    res.status(200).json(answer);
+  } else {
+    res.status(200).type('text').send(`Answer is ${req.query.length * req.query.length}`);
+  }
 });
 
 app.get('/*', (req,res) => {
-  res.set('Content-Type','text/html');
+  res.status(200).set('Content-Type','text/html');
   res.write('<html><body>');
-  res.write('<a href=/perimeter?length=3>Calcuate perimeter of a square with length = 3</a><br>');
-  res.write('<a href=/area?length=3>Calcuate area of a square with length = 3</a>');
+  res.write('<a href=/perimeter?length=3>Calculate perimeter of a square with length = 3</a><br>');
+  res.write('<a href=/area?length=3>Calculate area of a square with length = 3</a>');
   res.end('</body></html>')
 })
 
