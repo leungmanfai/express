7ab0dbabb78c2d770d55829ff43c95db2f25773d
Raymond So <rso@ouhk.edu.hk>
2019-10-15 11:18:12 +0800

    added default route handling

--- squares/server.js
+++ squares/server.js
@@ -1,13 +1,21 @@
 let express = require('express');
 let app = express();
 
-app.get('/perimeter', function (req, res) {
-  res.status(200).send(`Answer is ${req.query.length * 4}`);
+app.get('/perimeter', (req,res) => {
+  res.status(200).type('text').send(`Answer is ${req.query.length * 4}`);
 });
 
-app.get('/area', function (req, res) {
-  res.status(200).send(`Answer is ${req.query.length * req.query.length}`);
+app.get('/area', (req,res) => {
+  res.status(200).type('text').send(`Answer is ${req.query.length * req.query.length}`);
 });
+
+app.get('/*', (req,res) => {
+  res.set('Content-Type','text/html');
+  res.write('<html><body>');
+  res.write('<a href=/perimeter?length=3>Calcuate perimeter of a square with length = 3</a><br>');
+  res.write('<a href=/area?length=3>Calcuate area of a square with length = 3</a>');
+  res.end('</body></html>')
+})
 
 const server = app.listen(process.env.PORT || 8099, function () {
   const port = server.address().port;
