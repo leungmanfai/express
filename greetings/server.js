cb1659e2279200ed455bd1ec341b244a6a54c550
Raymond SO <rso@ouhk.edu.hk>
2019-11-04 19:14:01 +0800

    updated

--- greetings/server.js
+++ greetings/server.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const app = express();
 
-const greetingMsg = (name,showtime = false) => {
-	let today = new Date();
-	let msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello';
-	if (showtime) {
+const greetingMsg = (name, showtime = false) => {
+  let today = new Date();
+  let msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello';
+  if (showtime) {
     msg += `  It is now ${today.toTimeString()}`;
-	}
-	return(msg);
+  }
+  return (msg);
 }
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.redirect('/greetings');
 });
 
-app.get('/greetings', (req,res) => {
+app.get('/greetings', (req, res) => {
   res.send(greetingMsg(req.query.name));
 });
 
-app.get('/greetings/sayHelloWithTime', (req,res) => {
-  res.send(greetingMsg(req.query.name,true));
+app.get('/greetings/sayHelloWithTime', (req, res) => {
+  res.send(greetingMsg(req.query.name, true));
 });
 
-app.get('/*', (req,res) => {  // default route for anything else
+app.get('/*', (req, res) => {  // default route for anything else
   res.redirect('/greetings');
 })
 
