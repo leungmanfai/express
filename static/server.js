eb2e857127413fd5a4b01d9567b9f0e518d5e3b8
Raymond SO <rso@ouhk.edu.hk>
2019-11-05 19:06:42 +0800

    added virtual path

--- static/server.js
+++ static/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.static('assets'));
+app.use("/download",express.static('public/assets'));  // virtual path /download -> public/assets
 
 app.get('/',(req,res) => {
     res.render('index.html');
