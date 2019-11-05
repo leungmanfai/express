d9165fa9f4224cc409eb2b680b6fd147c0bfb741
Raymond SO <rso@ouhk.edu.hk>
2019-11-05 16:37:11 +0800

    added additional static path

--- static/server.js
+++ static/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('assets'));
 
 app.get('/',(req,res) => {
     res.render('index.html');
