eec0d60d0a5da94af15ecd6b4b7b618455a203dd
Raymond SO <rso@ouhk.edu.hk>
2020-11-20 10:20:43 +0800

    updated

--- greetings/server.js
+++ greetings/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-const greetingMsg = (name, showtime = false) => {
+const greetingMsg = (name = null, includeTime = false) => {
   let today = new Date();
-  let msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello';
-  if (showtime) {
+  let msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello there!';
+  if (includeTime) {
     msg += `  It is now ${today.toTimeString()}`;
+    msg = `<html><head><title>sayHello</title></head><body><H1>${msg}</H1></body><html>`;
   }
   return (msg);
 }
@@ -15,15 +16,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/greetings', (req, res) => {
-  res.send(greetingMsg(req.query.name));
+  res.set('Content-Type', 'text/html');
+  res.status(200).end(greetingMsg(req.query.name, false));
+});
+
+app.get('/greetings/sayHello', (req, res) => {
+  res.redirect('/greetings');
 });
 
 app.get('/greetings/sayHelloWithTime', (req, res) => {
-  res.send(greetingMsg(req.query.name, true));
+  res.set('Content-Type', 'text/html');
+  res.status(200).end(greetingMsg(req.query.name, true));
 });
 
 app.get('/*', (req, res) => {  // default route for anything else
-  res.redirect('/greetings');
+  res.set('Content-Type', 'text/plain');
+  res.status(404).end("404 Not Found");
 })
 
 const server = app.listen(process.env.PORT || 8099, () => {
